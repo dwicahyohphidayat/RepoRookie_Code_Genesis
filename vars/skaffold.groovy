c4e293788baed03aa452afca18d4f42f5e3bc12d
ais_admin <devops@ais.local>
2024-10-18 08:45:43 +0700

    ci: initial push

--- vars/skaffold.groovy
+++ vars/skaffold.groovy
@@ -1,4 +1,4 @@
 def call(String namespace) {
     echo "Deploying with Skaffold in namespace: ${namespace}..."
-    sh "skaffold run -f ${libraryResource('skaffold.yaml')} --namespace ${namespace}"
+    sh "skaffold run -f ${libraryResource('manifests/skaffold.yaml')} --namespace ${namespace}"
 }

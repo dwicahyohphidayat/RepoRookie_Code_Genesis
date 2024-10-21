482119aa63dfdd48f76268d5e892f260e96294d9
ais_admin <devops@ais.local>
2024-10-21 08:13:31 +0700

    ci: initial push --run-build

--- src/org/akarintitech/Preprocessor.groovy
+++ src/org/akarintitech/Preprocessor.groovy
@@ -1,22 +1,21 @@
 package org.akarintitech
 
-class Preprocessor {
-    static Map<String, String> preprocess(String repoUrl, String branch, int targetPort) {
-        def repoName = repoUrl.tokenize('/').last().replace('.git', '').replace('_', '-')
-        def firstWord = repoName.tokenize('-').first()
-        def remainingWords = repoName.tokenize('-').drop(1).join('-')
+static Map<String, String> preprocess(String repoUrl, String branch, int targetPort) {
+    def repoName = repoUrl.tokenize('/').last().replace('.git', '').replace('_', '-').toLowerCase()
+    def firstWord = repoName.tokenize('-').first().toLowerCase()
+    def remainingWords = repoName.tokenize('-').drop(1).join('-').toLowerCase()
 
-        def variables = [
-            AITPROJNAME: "${repoName}-${branch}",
-            AITGITCLIENT: firstWord,
-            AITGITSERVICE: remainingWords,
-            AITGITNAME: repoName,
-            AITGITBRANCH: branch,
-            AITTARGETPORT: targetPort.toString()
-        ]
+    def variables = [
+        AITPROJNAME: "${repoName}-${branch.toLowerCase()}",
+        AITGITCLIENT: firstWord,
+        AITGITSERVICE: remainingWords,
+        AITGITNAME: repoName,
+        AITGITBRANCH: branch.toLowerCase(),
+        AITTARGETPORT: targetPort.toString()
+    ]
 
-        return variables
-    }
+    return variables
+}
 
     static String replaceVariables(String content, Map<String, String> variables) {
         variables.each { key, value ->

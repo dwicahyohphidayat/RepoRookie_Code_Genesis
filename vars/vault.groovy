a9df7b55e4d36b9105913804741c2bb0c55b0b92
ais_admin <devops@ais.local>
2024-10-22 13:28:20 +0700

    ci: initial push

--- vars/vault.groovy
+++ vars/vault.groovy
@@ -1,13 +1,16 @@
 def call(String vaultPath, String initData) {
     println "Executing vault command"
-    println "PATH: ${System.getenv('PATH')}"
     println "Vault Path: ${vaultPath}"
     println "Init Data: ${initData}"
-    sh "vault --version"
 
     // Function to check if the Vault path exists
     def pathExists = { path ->
-        def process = "/usr/local/bin/vault kv get ${path}".execute()
+        def script = """
+        #!/bin/bash
+        export PATH=\$PATH:/usr/local/bin
+        vault kv get ${path}
+        """
+        def process = new ProcessBuilder("/bin/bash", "-c", script).start()
         process.waitFor()
         return process.exitValue() == 0
     }
@@ -15,13 +18,18 @@ def call(String vaultPath, String initData) {
     // Function to put data into Vault if the path does not exist
     def putDataIfNotExists = { path, data ->
         if (!pathExists(path)) {
-            def command = "/usr/local/bin/vault kv put ${path} ${data}"
-            def process = command.execute()
+            def script = """
+            #!/bin/bash
+            export PATH=\$PATH:/usr/local/bin
+            vault kv put ${path} ${data}
+            """
+            def process = new ProcessBuilder("/bin/bash", "-c", script).start()
             process.waitFor()
             if (process.exitValue() == 0) {
                 println "Data successfully written to ${path}"
             } else {
                 println "Failed to write data to ${path}"
+                println process.errorStream.text
             }
         } else {
             println "Vault path ${path} already exists. Doing nothing."

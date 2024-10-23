09b48a29adc282b6baf4971392da9da7cd6234a7
ais_admin <devops@ais.local>
2024-10-23 18:02:33 +0700

    ci: initial push

--- vars/akarintiPipeline.groovy
+++ vars/akarintiPipeline.groovy
@@ -1,24 +1,73 @@
 // vars/akarintiPipeline.groovy
 def call(Map config) {
 
-    def podTemplate = libraryResource('template/pod/build.yaml')    
-
-    pipeline {
-        agent {
-            kubernetes {
-                label 'eci-jenkins-agent'
-                yaml podTemplate
-            }
-        }
-        stages {
-            stage('Check Resource') {
-                steps {
-                    script {
-                        echo "Pod Template: ${podTemplate}"
-                    }
-                }
-            }
-            // Add your other stages here
+pipeline {
+    agent {
+        kubernetes {
+            label 'eci-jenkins-agent'
+            yaml """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              labels:
+                jenkins: agent
+            spec:
+              tolerations:
+              - key: virtual-kubelet.io/provider
+                operator: Equal
+                value: alibabacloud
+                effect: NoSchedule
+              affinity:
+                nodeAffinity:
+                  requiredDuringSchedulingIgnoredDuringExecution:
+                    nodeSelectorTerms:
+                    - matchExpressions:
+                      - key: kubernetes.io/hostname
+                        operator: In
+                        values:
+                        - virtual-kubelet-ap-southeast-5a
+                        - virtual-kubelet-ap-southeast-5b
+              imagePullSecrets:
+              - name: docker-registry-secret
+              containers:
+              - name: jnlp
+                image: aitops/tools:skaffold-vault
+                tty: true
+                env:
+                - name: KUBECONFIG
+                  value: /kubeconfig/kubeconfig
+                - name: VAULT_TOKEN
+                  valueFrom:
+                    secretKeyRef:
+                      name: hashicorp-secret
+                      key: vault-token
+                - name: VAULT_ADDR
+                  value: http://vault.vault.svc.cluster.local:8200
+                volumeMounts:
+                - name: kubeconfig-secret
+                  mountPath: /kubeconfig
+                resources:
+                  requests:
+                    cpu: "250m"
+                    memory: "256Mi"
+                  limits:
+                    cpu: "500m"
+                    memory: "512Mi"
+              volumes:
+              - name: kubeconfig-secret
+                secret:
+                  secretName: kubeconfig-secret
+            """
         }
     }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building...'
+            }
+        }
+    }
+}
+
+
 }

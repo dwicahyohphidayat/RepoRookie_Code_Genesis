36abeb730ff0deb6a591a8f4335b2673293ec669
ais_admin <devops@ais.local>
2024-10-24 12:48:17 +0700

    ci: initial push

--- vars/akarintiPipeline.groovy
+++ vars/akarintiPipeline.groovy
@@ -24,7 +24,7 @@ def call(Map config) {
                     expression { config.sonarscan == 'yes' }
                 }
                 steps {
-                    container('test') {
+                    container('jnlp') {
                         sonarscan(config)
                     }
                     script {
--- vars/selectPodTemplate.groovy
+++ vars/selectPodTemplate.groovy
@@ -1,11 +1,11 @@
 def call(Map config) {
     def podTemplate
-    if (config.sonarscan == 'no') {
+    if (!config.testImage) {
         podTemplate = libraryResource('template/pod/build.yaml')
-    } else if (config.sonarscan == 'yes' && config.tests?.integration?.enabled == 'no') {
+    } else if (config.tests?.unit?.enabled) {
         podTemplate = libraryResource('template/pod/build-unit.yaml')
         podTemplate = podTemplate.replace('${AITTESTIMAGE}', config.testImage)
-    } else if (config.sonarscan == 'yes' && config.tests?.integration?.enabled == 'yes') {
+    } else if (config.tests?.integration?.enabled) {
         podTemplate = libraryResource('template/pod/build-integration.yaml')
         podTemplate = podTemplate.replace('${AITTESTIMAGE}', config.testImage)
         podTemplate = podTemplate.replace('${AITDBTESTIMAGE}', config.dbTestImage)

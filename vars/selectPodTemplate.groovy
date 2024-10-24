97b3776355b962457ca43ccfc25cca28ade3f544
ais_admin <devops@ais.local>
2024-10-24 10:07:40 +0700

    ci: initial push

--- vars/selectPodTemplate.groovy
+++ vars/selectPodTemplate.groovy
@@ -4,11 +4,11 @@ def call(Map config) {
         podTemplate = libraryResource('template/pod/build.yaml')
     } else if (config.tests?.unit?.enabled) {
         podTemplate = libraryResource('template/pod/build-unit.yaml')
-        podTemplate = podTemplate.replace('${config.testImage}', config.testImage)
+        podTemplate = podTemplate.replace('${AITTESTIMAGE}', config.testImage)
     } else if (config.tests?.integration?.enabled) {
         podTemplate = libraryResource('template/pod/build-integration.yaml')
-        podTemplate = podTemplate.replace('${config.testImage}', config.testImage)
-        podTemplate = podTemplate.replace('${config.dbTestImage}', config.dbTestImage)
+        podTemplate = podTemplate.replace('${AITTESTIMAGE}', config.testImage)
+        podTemplate = podTemplate.replace('${AITDBTESTIMAGE}', config.dbTestImage)
     } else {
         podTemplate = libraryResource('template/pod/build.yaml') // Default fallback
     }
